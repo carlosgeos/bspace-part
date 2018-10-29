a1f50a8c3f2ca955d78348b8d1c6e6270757e459
Carlos Requena <crequena@ulb.ac.be>
2018-10-29 11:38:28 +0100

    make segments shorter

--- src/bspace_part/core.clj
+++ src/bspace_part/core.clj
@@ -1,9 +1,35 @@
 (ns bspace-part.core
+  (:gen-class)
   (:require [quil.core :as q])
-  (:gen-class))
+  (:import (java.util.concurrent ThreadLocalRandom)))
+
+(def MAX_SEGMENT_LENGTH 50)
+(def WIDTH 1080)
+(def HEIGHT 1080)
+
+(def rand_number_gen (ThreadLocalRandom/current))
+
+(defn segments_intersect?
+  "Returns a boolean that says if the two segments intersect or not"
+  [s1 s2]
+  )
+
+
+(defn random_epsilon
+  "Random integer in the range [-MAX_SEGMENT_LENGTH, MAX_SEGMENT_LENGTH]"
+  []
+  (.nextInt rand_number_gen (- MAX_SEGMENT_LENGTH) MAX_SEGMENT_LENGTH))
+
+(defn random_segment
+  "Return a random segment of max size MAX_SEGMENT_LENGTH. The return
+  format is [x1 y1 x2 y2]"
+  []
+  (let [x1 (rand-int WIDTH)
+        y1 (rand-int HEIGHT)]
+    [x1 y1 (+ x1 (random_epsilon)) (+ y1 (random_epsilon))]))
 
 (defn setup []
-  (q/frame-rate 60)                    ;; Set framerate to 1 FPS
+  (q/frame-rate 60)   ;; Set framerate to 1 FPS
   (q/background 255))                 ;; Set the background colour to
 ;; a nice shade of grey.
 (defn draw []
@@ -11,19 +37,16 @@
   (q/stroke-weight 2)       ;; Set the stroke thickness randomly
   (q/fill (q/random 255))               ;; Set the fill colour to a random grey
 
-  (let [x1    (q/random (q/width))
-        y1    (q/random (q/height))
-        x2    (q/random (q/width))
-        y2    (q/random (q/height))]
-    (q/line x1 y1 x2 y2)))         ;; Draw a circle at x y with the correct diameter
+  (let [[x1 y1 x2 y2] (random_segment)] ;unpack values
+    (q/line x1 y1 x2 y2)))
 
 (q/defsketch example                  ;; Define a new sketch named example
   :title "Binary Space Partitions"    ;; Set the title of the sketch
   :settings #(q/smooth 2)             ;; Turn on anti-aliasing
   :setup setup                        ;; Specify the setup fn
   :draw draw                          ;; Specify the draw fn
-  :size [1080 1080])                    ;; You struggle to beat the golden ratio
+  :size [WIDTH HEIGHT])
 
 
 (defn -main []
-  (prn "Hello"))
+  (prn "BSpace partitions"))
